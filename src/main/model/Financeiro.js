45a0a33d51c896b916a03caa92fdff0408a02a68
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2018-08-17 11:19:32 -0400

    Criação do Controller Financeiro

---
+++ src/main/model/Financeiro.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const Mongoose = require('mongoose');
+
+class Financeiro extends Mongoose.Schema {
+
+    constructor() {
+
+        super({
+
+            tipo: {
+                type: String,
+                enum: ["Despesa", "Receita"],
+                default: "Despesa",
+                required: true,
+
+            },
+
+            valor: {
+                type: Number,
+                min: 0,
+                required: true
+            },
+
+            dataDeCriacao: {
+                type: Date(),
+                default: new Date().getDate(),
+                required: true
+            },
+
+            estabelecimento: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Estabelecimento',
+                required: true
+            }
+
+        });
+
+        Mongoose.model('Financeiro', this);
+    } // contructor
+
+} // class
+
+new Financeiro();
+
+module.exports = Financeiro;
